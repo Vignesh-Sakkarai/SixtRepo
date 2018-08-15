162ea176d6d15dcc1bca4ccfd55f78e8a1ea4539
Vignesh-Sakkarai <31211400+Vignesh-Sakkarai@users.noreply.github.com>
2018-08-15 13:32:24 +0200

    Update Anagram.java

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -1,26 +1,44 @@
+package com.company.Eulers;
+
 import java.util.Arrays;
 
 public class Anagram {
-
     public static void main(String args[]){
-        String lhs = "dealer";
-        String rhs = "leader";
-        System.out.println("Result is: " + isAnagram(lhs, rhs));
+        String lhs = "leader";
+        String rhs = "dealer1";
+        System.out.println(isAnagram(lhs, rhs));
+        System.out.println(isAnagramOtherWay(lhs, rhs));
+    }
+
+    private static boolean isAnagramOtherWay(String lhs, String rhs) {
+        //They have to be similar length to achieve it as Anagrams
+        if(lhs.length() != rhs.length()){
+            return false;
+        }
+        String temp = rhs;
+        char[] lhsArray = lhs.toLowerCase().toCharArray();
+        //Check each character in a lhs String with rhs and if present remove the character from the rhs
+        for(int i=0; i<lhsArray.length; i++){
+            if(!temp.contains(String.valueOf(lhsArray[i]))){
+                return false;
+            }
+            temp = rhs.replaceFirst(String.valueOf(lhsArray[i]) , "");
+            rhs = temp;
+        }
+        return true;
     }
 
     private static boolean isAnagram(String lhs, String rhs) {
-        // the length should be equal for anagram
+        //They have to be similar length to achieve it as Anagrams
         if(lhs.length() != rhs.length()){
             return false;
         }
 
-        //Convert the String to Char array, do sort and check
+        //Sort the String based on the characters and then compare. If both are equal then its a Anagram
         char[] lhsArray = lhs.toLowerCase().toCharArray();
         char[] rhsArray = rhs.toLowerCase().toCharArray();
         Arrays.sort(lhsArray);
         Arrays.sort(rhsArray);
-        return Arrays.equals(lhsArray,rhsArray);
+        return Arrays.equals(lhsArray, rhsArray);
     }
-
-
 }
